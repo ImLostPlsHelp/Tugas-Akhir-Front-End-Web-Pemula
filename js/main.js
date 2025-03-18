15b88e9e2c932eff8d3fc34072ac9183111abf89
ImLostPlsHelp <107686662+ImLostPlsHelp@users.noreply.github.com>
2025-03-18 15:24:35 +0700

    Add functions to mark book completion and undo completion

--- js/main.js
+++ js/main.js
@@ -72,7 +72,7 @@ function saveData() {
 
 /**
  * Fungsi ini digunakan untuk memuat data dari localStorage
- * Dan memasukkan data hasil parsing ke variabel {@see todos}
+ * Dan memasukkan data hasil parsing ke variabel {@see books}
  */
 function loadDataFromStorage() {
   const serializedData /* string */ = localStorage.getItem(STORAGE_KEY);
@@ -86,3 +86,55 @@ function loadDataFromStorage() {
 
   document.dispatchEvent(new Event(RENDER_EVENT));
 }
+
+function addTaskToCompleted(bookId /* HTMLELement */) {
+  const bookTarget = findBook(bookId);
+
+  if (bookTarget == null) return;
+
+  bookTarget.isCompleted = true;
+  document.dispatchEvent(new Event(RENDER_EVENT));
+  saveData();
+}
+
+function removeTaskFromCompleted(bookId /* HTMLELement */) {
+  const bookTarget = findBookIndex(bookId);
+
+  if (bookTarget === -1) return;
+
+  books.splice(bookTarget, 1);
+  document.dispatchEvent(new Event(RENDER_EVENT));
+  saveData();
+}
+
+function undoTaskFromCompleted(bookId /* HTMLELement */) {
+
+  const bookTarget = findBook(bookId);
+  if (bookTarget == null) return;
+
+  bookTarget.isCompleted = false;
+  document.dispatchEvent(new Event(RENDER_EVENT));
+  saveData();
+}
+
+document.addEventListener(SAVED_EVENT, () => {
+  alert('Data berhasil disimpan');
+});
+
+document.addEventListener(RENDER_EVENT, function () {
+  const uncompletedBookList = document.getElementById('incompleteBookList');
+  const listCompleted = document.getElementById('completeBookList');
+
+  // clearing list item
+  uncompletedBookList.innerHTML = '';
+  listCompleted.innerHTML = '';
+
+  for (const bookItem of books) {
+    const bookElement = makeBook(bookItem);
+    if (bookItem.isCompleted) {
+      listCompleted.append(bookElement);
+    } else {
+      uncompletedBookList.append(bookElement);
+    }
+  }
+})
